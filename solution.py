55f0a606174006b7d8beab4420cbd02e64734e88
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 11:00:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import ttest_ind
 
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
@@ -8,15 +8,8 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    p_control = x_success / x_cnt
-    p_test = y_success / y_cnt
-
-    expected = [p_control * y_cnt, (1 - p_control) * y_cnt,
-    p_test * y_cnt, (1 - p_test) * y_cnt]
-
-    observed = [x_success, x_cnt - x_success,
-    y_success, y_cnt - y_success]
-
-    chi2, p_value = stats.chisquare(observed, expected)
-
-    return p_value < 0.08
+    alpha=0.08
+    control_conv = x_success / x_cnt
+    test_conv = y_success / y_cnt
+    t_stat, p_value = ttest_ind(control_conv, test_conv, equal_var=False)
+    return p_value < alpha
