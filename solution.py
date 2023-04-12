255d517bb2f71b62966274e8cd692bea731749ae
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 09:28:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.08
+    ctrl_conv_rate = x_success / x_cnt
+    test_conv_rate = y_success / y_cnt
+
+    t_stat, p_val = ttest_ind([ctrl_conv_rate], [test_conv_rate])
+
+    if p_val < alpha:
+        return True
+    else:
+        return False
